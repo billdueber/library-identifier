3b890f395454513e503b413240ccac6e571a9d55
Bill Dueber <bill@dueber.com>
2017-05-04 23:50:58 -0400

    Simplify the extractor in lieu of flexibility

--- lib/library/identifier/utils.rb
+++ lib/library/identifier/utils.rb
@@ -2,26 +2,36 @@ module Library
   module Identifier
     module Utils
 
-      SEPARATOR_CHARS = "\\s;,/"
-      ID_CHARS        = "\\dXx"
-      STUFFTOKEEP     = SEPARATOR_CHARS + ID_CHARS
-      DITCH           = Regexp.new "[^#{STUFFTOKEEP}]+"
-      SEPARATORS      = Regexp.new("[#{SEPARATOR_CHARS}]+")
-
+      DEFAULT_SEPARATOR = %r([\s;,/]+)
 
       extend self; # make everything a module function
 
-      def extract_digitstrings(str)
-        str.gsub(DITCH, '').
-          split(SEPARATORS).
-          map { |x| x.gsub(SEPARATORS, '') }.
-          compact.
-          delete_if { |x| x.empty? or x.nil? }
+      # A good extractor for ISBN/ISSN/OCLC
+      # @param str [String]  The raw string that you suspect may contain digitstrings
+      # @param separator [Regexp]  A regex used to split into multiple digitstrings
+      # @param allow_trailing_x [Boolean]  Whether the number can end in 'X'
+      # @param min [Number]  Smallest string allowed
+      # @param max [Number]  Largest string allowed
+      # @return [Array<String>] extracted digitstrings
+      def extract_digitstrings(str,
+                               separator: DEFAULT_SEPARATOR,
+                               allow_trailing_x: true,
+                               min: 0, max: 100)
+
+        scanner = if allow_trailing_x
+                    /\d+X?/
+                  else
+                    /\d+/
+                  end
+        str.upcase.gsub(/(\d)X(\d)/, '\1 \2').# any internal Xs need to be split on
+        split(separator).map {|x| x.gsub(/[^\dX]/, '').scan(scanner)}.flatten
       end
 
 
-      def extract_digitstring(str)
-        extract_digitstrings(str).first
+      # Get just the first digitstring
+      # @return String
+      def extract_digitstring(*args, **kwargs)
+        extract_digitstrings(*args, **kwargs).first
       end
 
 
--- spec/library/identifier/utils_spec.rb
+++ spec/library/identifier/utils_spec.rb
@@ -6,7 +6,7 @@ describe "Utility methods" do
   DS = ->(str) { U.extract_digitstring(str)}
   DSS = ->(str) { U.extract_digitstrings(str)}
 
-  describe "extract_digitstring" do
+  describe "digit string extraction" do
     it "leaves a set of digits alone" do
       DS['123456'].must_equal '123456'
     end
@@ -18,9 +18,7 @@ describe "Utility methods" do
     it "throws away garbage" do
       DS['ISBN: 123-455-4321 (pb.)'].must_equal '1234554321'
     end
-  end
 
-  describe "extract digitstrings" do
     it "finds multiple" do
       DSS['12345 and 7890'].must_equal ['12345', '7890']
     end
@@ -28,6 +26,20 @@ describe "Utility methods" do
     it "pulls out some complex stuff" do
       DSS['  (hb) 12345-67-890; 44/55'].must_equal ['1234567890', '44', '55']
     end
+
+    it "can handle and upcase an X" do
+      DS['  blah blah 12345x'].must_equal '12345X'
+    end
+
+    it "fails with an x in the middle of a string" do
+      DSS[' 123X45'].must_equal ['123', '45']
+    end
+
+    it "is ok with no matches" do
+      DS['hello world'].must_be_nil
+      DSS['hello world'].must_be_empty
+    end
+
 
   end
 
