f84bdf8e7d970f18e1f00569473251bc3aabc468
Bill Dueber <bill@dueber.com>
2017-05-19 00:10:16 -0400

    Update minitest::spec syntax

--- spec/library/identifier/utils_spec.rb
+++ spec/library/identifier/utils_spec.rb
@@ -7,20 +7,20 @@ describe "Utility methods" do
   describe "digit string extraction" do
     describe "basics" do
       it "leaves a set of digits alone" do
-        ME['123456'].must_equal ['123456']
+        value(ME['123456']).must_equal ['123456']
       end
 
       it "is ok with no matches" do
-        ME['hello world'].must_be_empty
+        value(ME['hello world']).must_be_empty
       end
 
       it "pulls out of the middle of a string" do
-        ME['   123   '].must_equal ['123']
+        value(ME['   123   ']).must_equal ['123']
       end
 
 
       it "finds multiple" do
-        ME['12345 and 7890'].must_equal ['12345', '7890']
+        value(ME['12345 and 7890']).must_equal ['12345', '7890']
       end
 
     end
@@ -29,12 +29,12 @@ describe "Utility methods" do
       ISBN = Library::Identifier::ISBNExtractor
 
       it "throws away garbage" do
-        ISBN['ISBN: 123-455-4321 (pb.)'].must_equal ['1234554321']
+        value(ISBN['ISBN: 123-455-4321 (pb.)']).must_equal ['1234554321']
       end
 
 
       it "pulls out some complex stuff" do
-        ISBN['  (hb) 12345-67-890; 44/55 123456789X'].must_equal ['1234567890', '123456789X']
+        value(ISBN['  (hb) 12345-67-890; 44/55 123456789X']).must_equal ['1234567890', '123456789X']
       end
 
     end
